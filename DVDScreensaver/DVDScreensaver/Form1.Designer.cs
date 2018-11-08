daacc385822d544758cbc0ecb3d841b8b6ebab24
André <andrerrr@outlook.de>
2018-11-08 10:56:48 +0100

    Bester DVD Scrensaver ever!

--- DVDScreensaver/DVDScreensaver/Form1.Designer.cs
+++ DVDScreensaver/DVDScreensaver/Form1.Designer.cs
@@ -29,18 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new DVDScreensaver.MeinButton();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(115, 44);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(64, 35);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "DVD";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // timer1
             // 
@@ -48,12 +39,22 @@
             this.timer1.Interval = 1;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(131, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(375, 212);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "meinButton1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(383, 285);
+            this.ClientSize = new System.Drawing.Size(977, 401);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -61,9 +62,8 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
+        private MeinButton button1;
     }
 }
 
---
+++ DVDScreensaver/DVDScreensaver/MeinButton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVDScreensaver
+{
+    class MeinButton : Button
+    {
+        int count = 0;
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+
+            pevent.Graphics.FillRectangle(new SolidBrush(Parent.BackColor), ClientRectangle);
+            pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            pevent.Graphics.FillEllipse(new LinearGradientBrush(ClientRectangle, BackColor, Parent.BackColor, 90),
+                ClientRectangle);
+
+
+            if (ClientRectangle.Contains(PointToClient(Cursor.Position)))
+                pevent.Graphics.DrawEllipse(new Pen(Color.Silver, 4), ClientRectangle);
+
+            StringFormat sf = new StringFormat()
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+            pevent.Graphics.DrawString($"DVD {count++}", new Font("Ink Free", 26, FontStyle.Bold),
+                Brushes.OrangeRed, ClientRectangle, sf);
+        }
+    }
+}
